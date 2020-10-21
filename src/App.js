87466d761440fb5a10b18e0f8690c8cf853a729f
Corey Johnson <cjmkori90@gmail.com>
2020-10-21 14:46:27 +0300

    Added google analytics to my app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React from "react";
+import ReactGA from "react-ga"; 
+import { createBrowserHistory } from 'history';
 import './App.css';
 import { Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
 import Main from './components/main'
 import { Link } from 'react-router-dom';
 
 function App() {
+
+    const history = createBrowserHistory();
+
+    // Initialize google analytics page view tracking
+    history.listen(location => {
+      ReactGA.initialize('G-60J3B94QPR');
+      ReactGA.set({ page: location.pathname }); // Update the user's current page
+      ReactGA.pageview(location.pathname); // Record a pageview for the given page
+    });
+
   return (
     <div>
     <Layout>
